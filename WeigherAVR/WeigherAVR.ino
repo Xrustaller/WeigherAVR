db4da5e1830a4e9d9700ca6e99041a97a9ae9321
Xrustaller <stalker0077@gmail.com>
2022-07-25 02:21:10 +0300

    v2.1 MODBUS

--- WeigherAVR/WeigherAVR.ino
+++ WeigherAVR/WeigherAVR.ino
@@ -1,42 +1,47 @@
-#define BTN_PIN_TAREE 6
-#define BEEP_PIN 10  // ѕин пьезо (при выборе 9 пина, 10 - недоступен из-за шим)
+#define BTN_PIN_TAREE 4
+#define BEEP_PIN 9  // Пин пьезо (при выборе 9 пина, 10 - недоступен из-за шим)
 
 #define DT_PIN_TAREE 3
 #define SCK_PIN_TAREE 2
 
-#define SOFT_UART_SPEED 1000 // —корость Soft-UART
-#define SOFT_UART_PIN 13 // ѕин
-#define BUS_ADRESS 0 // јдрес
-#define SOFT_UART_BUFFER 20 // Ѕуфер 
+#define DISP_UPDATE_FRAN 200 // Частота обновления дисплея (мс)
+
+#define CALIBRATION 2005.0 // 848.0
+
+#define BUS_ID 7
+#define PIN_REDE A1
 
 #include <GyverButton.h>
 #include <HX711_ADC.h>
 #include <DTM1650.h>
-#include <TIMECLASS.h>
-#include <softUART.h>
-#include <GBUS.h>
+#include <ModbusRtu.h>
 
 GButton button_tare(BTN_PIN_TAREE);
 
 HX711_ADC LoadCell(DT_PIN_TAREE, SCK_PIN_TAREE); // parameters: dt pin, sck pin
 DTM1650 display;
 
-softUART<SOFT_UART_PIN> main_soft_uart(SOFT_UART_SPEED);
-GBUS main_bus(&main_soft_uart, BUS_ADRESS, SOFT_UART_BUFFER);
+Modbus bus(BUS_ID, 0, PIN_REDE);
+int8_t state = 0;
+
+uint16_t temp[1] = { 0 };
 
 float i;
 
+unsigned long display_update;
+
 // the setup function runs once when you press reset or power the board
-void setup()
-{
+void setup(){
 	pinMode(BEEP_PIN, OUTPUT);
+
+	bus.begin(19200);
 
 	display.init();
 	display.set_brightness(DTM1650_BRIGHTNESS_MAX);
 
 	LoadCell.begin(); // start connection to HX711
 	LoadCell.start(1000); // load cells gets 2000ms of time to stabilize
-	LoadCell.setCalFactor(-392.24); // calibration factor for load cell => strongly dependent on your individual setup
+	LoadCell.setCalFactor(CALIBRATION); // calibration factor for load cell => strongly dependent on your individual setup
 
 	display.send_digit(DTM1650_S_MINUS, 0);
 	display.send_digit(DTM1650_S_MINUS, 1);
@@ -50,35 +55,45 @@ void setup()
 // the loop function runs over and over again until power down or reset
 void loop()
 {
+	state = bus.poll(temp, 2);
 	button_tare.tick();
 
 	LoadCell.update(); // retrieves data from the load cell
 	i = LoadCell.getData(); // get output value
+	if (i < 0) i = 0;
 
-	display.write_num(i);
+	temp[0] = i;
 
-	if (i >= 1000)
+	if (millis() - display_update > DISP_UPDATE_FRAN)
 	{
-		i = 0;
-		tone(BEEP_PIN, 1600, 200);
-		display.set_dot(3, true);
-	}
-	else
-	{
-		display.set_dot(3, false);
+		display_update = millis();
+		
+		if (i > 1000)
+		{
+			i = 0;
+			tone(BEEP_PIN, 1600, 200);
+			display.send_digit(DTM1650_S_MINUS, 0);
+			display.send_digit(DTM1650_S_MINUS, 1);
+			display.send_digit(DTM1650_S_MINUS, 2);
+			display.send_digit(DTM1650_S_MINUS, 3);
+		}
+		else
+		{
+			display.write_num(i);
+		}
 	}
 
 	if (button_tare.isPress())
 	{
-		LoadCell.start(1000);
-		display.set_dot(3, true);
 		display.send_digit(DTM1650_S_MINUS, 0);
 		display.send_digit(DTM1650_S_MINUS, 1);
 		display.send_digit(DTM1650_S_MINUS, 2);
 		display.send_digit(DTM1650_S_MINUS, 3);
+		LoadCell.start(1000);
+		display.set_dot(3, true);
 		tone(BEEP_PIN, 1000, 300);
 		delay(300);
 	}
 
-	main_bus.tick();
+	//main_bus.tick();
 }
